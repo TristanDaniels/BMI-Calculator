c3b3d7d3256addb00d61558641c68cf360aa6fbb
TristanDaniels <tristandaniels07@gmail.com>
2022-06-15 10:09:13 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
+let comment = document.getElementById("comment");
 function bmicalc() {
   let weight = document.getElementById("weight").value;
   let height = document.getElementById("height").value;
   let answer = weight / height ** 2;
   console.log(answer);
   document.querySelector("#answer").innerHTML = answer;
+  if (answer < 18) {
+    comment.innerText = "underweight";
+  } else if (answer >= 18 && answer < 25) {
+    comment.innerText = "normal";
+  } else if (answer >= 25 && answer < 30) {
+    comment.innerText = "overweight";
+  } else if (answer > 30) comment.innerText = "obese";
 }
